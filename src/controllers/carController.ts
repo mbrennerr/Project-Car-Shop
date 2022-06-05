8b1a020c34f5d6aaf2113c0a3df441252876400a
mbrennerr <marciobrennerbusiness@gmail.com>
2022-06-05 20:06:25 -0300

    feat:creat_car_controller

---
+++ src/controllers/carController.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express';
+import { Car } from '../interfaces/CarInterface';
+import genericController from './genericController';
+import CarService from '../service/carService';
+
+export default class CarController extends genericController<Car> {
+  constructor(carService = new CarService()) {
+    super(carService);
+  }
+
+  create = async (req:Request, res:Response, next:NextFunction) => {
+    try {
+      const car:Car = req.body; 
+      const newCar:Car | null = await this.Service.create(car);
+      return res.status(201).json(newCar);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
