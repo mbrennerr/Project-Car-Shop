e2f3e3d9d3924d65294dbf95ede08fe752dd8a9e
mbrennerr <marciobrennerbusiness@gmail.com>
2022-06-05 16:35:36 -0300

    Chore:change_interface_params;

--- src/interfaces/ModelInterface.ts
+++ src/interfaces/ModelInterface.ts
@@ -1,5 +1,5 @@
 export default interface Model<T>{
-  create(value: T): Promise<T>;
+  create(object: T): Promise<T>;
   read():Promise<T[]>;
   readOne(value: string): Promise<T | null>;
   update(value:string, object:T):Promise<T | null>;
