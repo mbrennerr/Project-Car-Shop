2e269da08c4a33ef3dea1afccb2c5063c9bc622f
mbrennerr <marciobrennerbusiness@gmail.com>
2022-06-05 20:05:14 -0300

    Chore:Refact_generic_controller;

--- src/controllers/genericController.ts
+++ src/controllers/genericController.ts
@@ -1,46 +1,37 @@
 import { NextFunction, Request, Response } from 'express';
-import Service from '../service/genericService';
+import GenericService from '../service/genericService';
 
-export type ResponseError = {
-  error: unknown;
-};
-enum ControllerErrors {
-  internal = 'Internal Server Error',
-  notFound = 'Object not found',
-  requiredId = 'Id is required',
-  badRequest = 'Bad request',
-}
+export default abstract class GenericController<T> {
+  protected Service:GenericService<T>;
 
-export default abstract class Controller<T> {
-  abstract route: string;
-
-  protected erros = ControllerErrors;
-
-  constructor(protected service: Service<T>) {}
+  // constructor(protected Service:GenericService<T>){}
+  constructor(serviço:GenericService<T>) {
+    this.Service = serviço;
+  }
 
   abstract create(
     req: Request,
-    res: Response<T | ResponseError>,
+    res: Response<T>,
     next: NextFunction
   ): Promise<typeof res | void>;
-  abstract read(
-    req: Request,
-    res: Response<T[] | ResponseError>,
-    next: NextFunction
-  ): Promise<typeof res | void>;
-  abstract readOne(
-    req: Request,
-    res: Response<T | ResponseError>,
-    next: NextFunction
-  ): Promise<typeof res | void>;
-  abstract update(
-    req: Request,
-    res: Response<T | ResponseError>,
-    next: NextFunction
-  ): Promise<typeof res | void>;
-  abstract delete(
-    req: Request,
-    res: Response<T | ResponseError>,
-    next: NextFunction
-  ): Promise<typeof res | void>;
+  // abstract read(
+  //   req: Request,
+  //   res: Response<T[] | ResponseError>,
+  //   next: NextFunction
+  // ): Promise<typeof res | void>;
+  // abstract readOne(
+  //   req: Request,
+  //   res: Response<T | ResponseError>,
+  //   next: NextFunction
+  // ): Promise<typeof res | void>;
+  // abstract update(
+  //   req: Request,
+  //   res: Response<T | ResponseError>,
+  //   next: NextFunction
+  // ): Promise<typeof res | void>;
+  // abstract delete(
+  //   req: Request,
+  //   res: Response<T | ResponseError>,
+  //   next: NextFunction
+  // ): Promise<typeof res | void>;
 } 
