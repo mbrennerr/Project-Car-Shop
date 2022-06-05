9400c3f16e56ef720fe476a27d18dc430dff047d
mbrennerr <marciobrennerbusiness@gmail.com>
2022-06-05 17:17:46 -0300

    Chore:changed_arquives;

--- src/interfaces/IModel.ts
+++
@@ -1,7 +0,0 @@
-export default interface Model<T>{
-  create(value: T): Promise<T>;
-  read():Promise<T[]>;
-  readOne(value: string): Promise<T | null>;
-  update(value:string, object:T):Promise<T | null>;
-  delete(value:string):Promise<T | null>;
-}
