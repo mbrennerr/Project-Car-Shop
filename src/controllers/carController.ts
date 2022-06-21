0316fed33da1cbfa1b2e0dfd99df28e9fed88f89
mbrennerr <marciobrennerbusiness@gmail.com>
2022-06-21 19:35:09 -0300

    Feat[Controller]:add_method_create;

--- src/controllers/carController.ts
+++ src/controllers/carController.ts
@@ -1,20 +1,29 @@
-import { Request, Response, NextFunction } from 'express';
+import { Response, NextFunction, Request } from 'express';
 import { Car } from '../interfaces/CarInterface';
-import genericController from './genericController';
+import Controller from './genericController';
 import CarService from '../service/carService';
 
-export default class CarController extends genericController<Car> {
-  constructor(carService = new CarService()) {
+export default class CarController extends Controller<Car> {
+  private _route:string;
+
+  constructor(
+    carService = new CarService(),
+    route = '/cars',
+  ) {
     super(carService);
+    this._route = route;
+    this.read = this.read.bind(this);
   }
 
+  get route() { return this._route; }
+
   create = async (req:Request, res:Response, next:NextFunction) => {
+    const { body } = req.body; 
     try {
-      const car:Car = req.body; 
-      const newCar:Car | null = await this.Service.create(car);
+      const newCar:Car | null = await this.Service.create(body);
       return res.status(201).json(newCar);
     } catch (error) {
       next(error);
     }
-  };  
+  };
 }
