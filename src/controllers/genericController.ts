35a790de909774393477d45b9b095e85416e6808
mbrennerr <marciobrennerbusiness@gmail.com>
2022-06-21 19:32:08 -0300

    Feat[Controller]:creates_helpers_method_read;

--- src/controllers/genericController.ts
+++ src/controllers/genericController.ts
@@ -1,7 +1,25 @@
 import { NextFunction, Request, Response } from 'express';
 import GenericService from '../service/genericService';
 
+export type ResponseError = {
+  error:unknown;
+};
+
+export interface RequestWithBody<T> extends Request {
+  body: T;
+} 
+
+enum ControllerErrors {
+  internal = 'Internal Server Error',
+  notFound = 'Object not found',
+  requiredId = 'Id is required',
+  badRequest = 'Bad Request',
+}
 export default abstract class GenericController<T> {
+  abstract route: string;
+
+  protected errors = ControllerErrors;
+
   protected Service:GenericService<T>;
 
   // constructor(protected Service:GenericService<T>){}
@@ -10,28 +28,38 @@ export default abstract class GenericController<T> {
   }
 
   abstract create(
-    req: Request,
-    res: Response<T>,
+    req: RequestWithBody<T>,
+    res: Response<T | ResponseError>,
     next: NextFunction
   ): Promise<typeof res | void>;
-  // abstract read(
-  //   req: Request,
-  //   res: Response<T[] | ResponseError>,
-  //   next: NextFunction
-  // ): Promise<typeof res | void>;
+
+  read = async (
+    _req: Request,
+    res: Response<T[] | ResponseError>,
+    
+  ): Promise<typeof res | void> => {
+    try {
+      const objs = await this.Service.read();
+      return res.json(objs);
+    } catch (err) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
+  // o Método read ja vai ficar implementado por default;
+
   // abstract readOne(
   //   req: Request,
-  //   res: Response<T | ResponseError>,
+  //   res: Response<T>,
   //   next: NextFunction
   // ): Promise<typeof res | void>;
   // abstract update(
   //   req: Request,
-  //   res: Response<T | ResponseError>,
+  //   res: Response<T>,
   //   next: NextFunction
   // ): Promise<typeof res | void>;
   // abstract delete(
   //   req: Request,
-  //   res: Response<T | ResponseError>,
+  //   res: Response<T>,
   //   next: NextFunction
   // ): Promise<typeof res | void>;
 } 
