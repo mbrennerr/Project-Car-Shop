f4eac9bd988da3ea3d6670b88ff388d6ceb5e686
mbrennerr <marciobrennerbusiness@gmail.com>
2022-06-23 17:04:08 -0300

    Chore[controller]:add_readOne_method;

--- src/controllers/carController.ts
+++ src/controllers/carController.ts
@@ -26,4 +26,22 @@ export default class CarController extends Controller<Car> {
       next(error);
     }
   };
+
+  readOne = async (req:Request, res:Response, next:NextFunction) => {
+    const { id } = req.params;
+    try {
+      if (id.length < 24) {
+        return res
+          .status(400)
+          .json({ error: 'Id must have 24 hexadecimal characters' });
+      }
+      const car:Car | null = await this.Service.readOne(id);
+      if (!car) {
+        return res.status(404).json({ error: 'Object not found' });
+      } 
+      return res.status(200).json(car);
+    } catch (error) {
+      next(error);
+    }
+  };
 }
