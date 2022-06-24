77a110ebc49b579676c10ff50a65c707a301d31e
mbrennerr <marciobrennerbusiness@gmail.com>
2022-06-24 01:27:49 -0300

    Chore[router]:add_motorcycle_routes;

--- src/routes/CustomRouter.ts
+++ src/routes/CustomRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import Controller from '../controllers/genericController';
 import Validate from '../middlewares/Validate';
+import MotoController from '../controllers/motoController';
 
 export default class CustomRouter<T> {
   public router:Router;
@@ -16,10 +17,19 @@ export default class CustomRouter<T> {
     controller:Controller<T>,
     route:string = controller.route,
   ) {
-    this.router.get(route, controller.read);
-    this.router.get(`${route}/:id`, controller.readOne);
-    this.router.post(route, this.validate.Car, controller.create);
-    this.router.put(`${route}/:id`, this.validate.Car, controller.update);
-    this.router.delete(`${route}/:id`, controller.delete);
-  }
+    if (controller instanceof MotoController) {
+      this.router.post(route, this.validate.Motorcycle, controller.create);
+      this.router
+        .put(`${route}/:id`, this.validate.Motorcycle, controller.update);
+      this.router.get(`${route}/:id`, controller.readOne);
+      this.router.get(route, controller.read);
+      this.router.delete(`${route}/:id`, controller.delete);
+    } else {
+      this.router.get(route, controller.read);
+      this.router.get(`${route}/:id`, controller.readOne);
+      this.router.post(route, this.validate.Car, controller.create);
+      this.router.put(`${route}/:id`, this.validate.Car, controller.update);
+      this.router.delete(`${route}/:id`, controller.delete);
+    }
+  } 
 }
