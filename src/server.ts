c3d2110a28c2e567812b35205dd2bef5ad456fd1
mbrennerr <marciobrennerbusiness@gmail.com>
2022-06-24 00:24:43 -0300

    Feat[Server]:add_moto_routes;

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
 import CarController from './controllers/carController';
+import MotoController from './controllers/motoController';
 import { Car } from './interfaces/CarInterface';
+import { Motorcycle } from './interfaces/MotorcycleInterface';
 import CustomRouter from './routes/CustomRouter';
 import App from './app';
 
 const server = new App();
 
 const carController = new CarController();
-
 const carRouter = new CustomRouter<Car>();
 carRouter.addRoute(carController);
 
+const motoController = new MotoController();
+const motoRouter = new CustomRouter<Motorcycle>();
+motoRouter.addRoute(motoController);
+
 server.addRouter(carRouter.router);
+server.addRouter(motoRouter.router);
 
 server.addmiddleware();
 
