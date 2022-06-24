ba835cdd2074e928ce81c92e11c4142dc5e9a8d0
mbrennerr <marciobrennerbusiness@gmail.com>
2022-06-23 23:26:00 -0300

    Chore[Route]:update_routes;

--- src/routes/CustomRouter.ts
+++ src/routes/CustomRouter.ts
@@ -16,9 +16,10 @@ export default class CustomRouter<T> {
     controller:Controller<T>,
     route:string = controller.route,
   ) {
-    this.router.post(route, this.validate.Car, controller.create);
     this.router.get(route, controller.read);
     this.router.get(`${route}/:id`, controller.readOne);
+    this.router.post(route, this.validate.Car, controller.create);
     this.router.put(`${route}/:id`, this.validate.Car, controller.update);
+    this.router.delete(`${route}/:id`, controller.delete);
   }
 }
